d6bc6dbb764288515389079293f848e19efc0768
aleksandr.pogozhii <aleksandr.pogozhii@devexpress.com>
2023-11-20 17:25:32 +0000

    catalog api

---
+++ FoodShop.Api.Catalog/Controllers/CatalogController.cs
@@ -0,0 +1,25 @@
+﻿using FoodShop.Core.Models;
+using FoodShop.Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodShop.Api.Catalog.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly FoodShopDbContext _db;
+
+        public CatalogController(FoodShopDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return _db.Products.Take(10);
+        }
+    }
+}
