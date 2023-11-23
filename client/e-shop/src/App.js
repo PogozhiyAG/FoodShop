a5a6d6d7ef5097529fafb0a75ab207488bd63a96
aleksandr.pogozhii <aleksandr.pogozhii@devexpress.com>
2023-11-23 20:31:50 +0000

    client draft

--- client/e-shop/src/App.js
+++ client/e-shop/src/App.js
@@ -1,9 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from './context/AuthContext';
+import Layout from './Layout';
 import Category from './Category';
 import Product from './Product';
-import Layout from './Layout';
 import Home from './Home';
 import NoPage from './NoPage';
 import Basket from './Basket'
@@ -11,7 +11,7 @@ import Basket from './Basket'
 
 const App = () => {
   return (    
-    <React.StrictMode>
+    
       <AuthProvider>
         <BrowserRouter>
           <Routes>
@@ -25,7 +25,7 @@ const App = () => {
           </Routes>
         </BrowserRouter>
       </AuthProvider>
-    </React.StrictMode>
+    
   );
 }
 
--- client/e-shop/src/Home.js
+++ client/e-shop/src/Home.js
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
-    return <h1>Home</h1>;
-  };
-  
-  export default Home;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://localhost:62788/Catalog')
+    .then(r => r.json())
+    .then(r => setProducts(r))
+  }, []);
+
+
+  return (
+    <>
+      <h1>Home</h1>
+      {products.map(i => <div>{i.name}</div>)}
+    </>
+  );
+};
+
+export default Home;
