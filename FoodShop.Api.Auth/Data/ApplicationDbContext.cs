e5bb6e54baab57a9a660aa0837670709a12fe9f3
aleksandr.pogozhii <aleksandr.pogozhii@devexpress.com>
2023-11-29 12:57:57 +0000

    save

---
+++ FoodShop.Api.Auth/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using FoodShop.Api.Auth.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodShop.Api.Auth.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+}
