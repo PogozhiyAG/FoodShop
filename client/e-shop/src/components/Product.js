6fb9523749f7ac2d31833f23cc79d85c772fb798
aleksandr.pogozhii <aleksandr.pogozhii@devexpress.com>
2023-12-20 20:03:30 +0000

    style

--- client/e-shop/src/components/Product.js
+++ client/e-shop/src/components/Product.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import useBasketContext from "../hooks/useContextBasket";
 
 
@@ -12,32 +13,39 @@ const Product = ({product}) => {
         basket.addToBasket(product.id, -1);
     }
 
-    const basketQuantity = basket.getPosition(product.id)?.quantity;
-    const productItemStyle = `${basketQuantity ? 'basket-added-product' : ''}  p-3 product-item`;
+    const basketQuantity = basket.getPosition(product.id)?.quantity;    
 
     return(
-        <div className="col-xxl-2 col-xl-3 col-lg-4 col-md-6 col-sm-12">
-            <div className={productItemStyle}>
+        <div className="col-xxl-2 col-xl-3 col-lg-4 col-md-6 col-sm-12 product-item-outer">            
+            <div className="product-item-inner">
+                <div className="product-item-header">
+
+                </div>
                 <img src="food.jpg" className="product-image"/>
-                <div className="product-item-description">{product.name}</div>
-                <div>{product.popularity}</div>
-                <div className="product-item-price">{product.price}</div>
+                <div className="product-item-info p-3">
+                    <div className="product-item-description fw-500">
+                        <Link>{product.name}</Link>
+                    </div>
+                    
+                    <div className="product-item-price fs-4 mb-2">£{product.price.toFixed(2)}</div>
+                    
+                    {
+                        basketQuantity 
+                            ? (
+                                <div className="d-flex justify-content-between">                            
+                                    <button className="basket-button" onClick={handleRemoveToBasket}>-</button>
+                                    <span className="m-2">{basketQuantity}</span>
+                                    <button className="basket-button" onClick={handleAddToBasket}>+</button>
+                                </div>
+                            )
+                            : (
+                                <div className="d-flex justify-content-between">
+                                    <button className="basket-button w-100" onClick={handleAddToBasket}>Add</button>
+                                </div>
+                            )
+                    }
+                </div>
                 
-                {
-                    basketQuantity 
-                        ? (
-                            <div className="d-flex justify-content-between">                            
-                                <button className="basket-button" onClick={handleRemoveToBasket}>-</button>
-                                <span className="m-2">{basketQuantity}</span>
-                                <button className="basket-button" onClick={handleAddToBasket}>+</button>
-                            </div>
-                        )
-                        : (
-                            <div className="d-flex justify-content-between">
-                                <button className="basket-button w-100" onClick={handleAddToBasket}>Add</button>
-                            </div>
-                        )
-                }
             </div>
         </div>
     );
