9ce57632b867f6925865d5213c9d89f17c2ddf88
aleksandr.pogozhii <aleksandr.pogozhii@devexpress.com>
2024-02-04 12:47:14 +0000

    stripe web hook

---
+++ FoodShop.Api.Order/Controllers/StripeWebHookController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stripe;
+
+namespace FoodShop.Api.Order.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class StripeWebHookController : ControllerBase
+{
+
+    [HttpPost]
+    public async Task<IActionResult> Post()
+    {
+        var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+        try
+        {
+            var stripeEvent = EventUtility.ParseEvent(json, false);
+
+            // Handle the event
+            Console.WriteLine("Unhandled event type: {0}", stripeEvent.Type);
+            Console.WriteLine($"Id: {(stripeEvent.Data.Object as IHasId).Id}");
+
+            return Ok();
+        }
+        catch (StripeException e)
+        {
+            return BadRequest();
+        }
+    }
+}
